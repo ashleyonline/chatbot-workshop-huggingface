5da6268372127a4b8e1b9a06cda853b745f3a5a4
ashleyonline <ugotpoop@gmail.com>
2026-02-11 13:18:45 -0800

    Update streamlit_app.py to customize

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,15 +13,15 @@ import nltk
 def get_stopwords():
     nltk.download('stopwords')
 
-st.set_page_config(page_title="Chat with a friend on the works of Rabindranath Tagore", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("Chat with a friend on the works of Rabindranath Tagore")
+st.set_page_config(page_title="Chat with a professor on DIY-ing your own digital media art education", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.title("Chat with a professor on DIY-ing your own digital media art education")
 
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": "Ask me a question about Rabindranath Tagore!!",
+            "content": "Ask me a question about DIY-ing your own digital media art education!!",
         }
     ]
 
@@ -44,11 +44,11 @@ def load_data():
     token=st.secrets.hftoken,
     generate_kwargs={"temperature": 0.7, "top_k": 50, "top_p": 0.95},
     provider="auto",  # this will use the best provider available
-    system_prompt="""You are an expert on the work of Rabindranath Tagore.
-    Answer the question using the provided documents, which contain relevant excerpts from the work of Rabindranath Tagore.
-    The context for all questions is the work of Rabindranath Tagore. Whenever possible, include a quotation from the provided excerpts of his work to illustrate your point.
-    Respond using a florid but direct tone, typical of an early modernist writer.
-    Respond in fewer than 100 words.""",
+    system_prompt="""You are an expert digital media art pedagogy at the post-secondary level.
+    Answer the question using the provided documents, which contain syllabi from a variety of studio, theory, and history courses.
+    The context for all questions is the fine arts discipline of digital media art. Whenever possible, provide in-line citations for your answers in MLA style. 
+    Respond using a direct but academic tone, typical of undergraduate or masters-level teaching.
+    Respond in fewer than 100 words, unless you are asked to create a full syllabus.""",
     )
     index = VectorStoreIndex.from_documents(docs)
     return index
